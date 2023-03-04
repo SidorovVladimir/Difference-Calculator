4c380f81f0e02dcef8be6d416beb1a9daeb5cd90
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2023-03-05 00:27:20 +0700

    add formatter stylish and function getTree

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,9 @@
 import formatStylish from './stylish.js';
-import formatPlain from './plain.js';
 
 const formatter = (tree, formatName) => {
   switch (formatName) {
     case 'stylish':
       return formatStylish(tree);
-    case 'plain':
-      return formatPlain(tree);
     default:
       throw new Error(`'unknown formatter! ${formatName}'`);
   }
